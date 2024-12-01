922a4cd49acb0ef5a1cf41f310d52f9b35e61245
Julian Parker-Sartori <132828842+becausej@users.noreply.github.com>
2024-12-01 18:17:15 -0500

    Deleted venv, updated gitignore

--- ChromeExtension/FlaskServer/routes.py
+++ ChromeExtension/FlaskServer/routes.py
@@ -3,13 +3,15 @@ import newspaper
 import numpy as np
 from backend_logic.google_sentiment import get_max_sentence, get_sentiment_values
 from backend_logic.analyze_sentence import predict_sentence
-
+from backend_logic.doomed import get_knn_class
+from backend_logic.smog import get_nela_smog
+from backend_logic.allsides import get_allsides
 bp = Blueprint('routes', __name__)
 
 # Load glove embeddings
 glove_embeddings = {}
 print("Loading glove embeddings")
-with open('backend_logic/glove.6B.100d.txt', 'r') as f:
+with open('backend_logic/glove.6B.100d.txt', 'r',encoding='utf8') as f:
     for line in f:
         values = line.split(' ')
         word = values[0]
@@ -36,6 +38,24 @@ def classify():
     if sentence.strip() == '' or sentence is None:
         return 0
     return predict_sentence(sentence, glove_embeddings)
+
+@bp.route('/classify_full_text', methods=['POST'])
+def classify_text():
+    data = request.get_json()
+    url = data.get('url', '')
+    return get_knn_class(url)
+
+@bp.route('/smog_full_text', methods=['POST'])
+def smog_text():
+    data = request.get_json()
+    url = data.get('url', '')
+    return get_nela_smog(url)
+
+@bp.route('/allsides_rating', methods=['POST'])
+def allsides_text():
+    data = request.get_json()
+    url = data.get('url', '')
+    return get_allsides(url)
 
 def sentiment_json(doc_sentiment, sentence, sentence_score):
     return jsonify({'total_sentiment': doc_sentiment, 
