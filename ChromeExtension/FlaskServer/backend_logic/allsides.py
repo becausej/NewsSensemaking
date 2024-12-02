2b0c096fd3a22b4dd2d505c98a7cfd68b79be5c0
Julian Parker-Sartori <132828842+becausej@users.noreply.github.com>
2024-12-01 21:51:32 -0500

    FIXED

--- ChromeExtension/FlaskServer/backend_logic/allsides.py
+++ ChromeExtension/FlaskServer/backend_logic/allsides.py
@@ -5,6 +5,7 @@ from flask import jsonify
 
 def get_allsides(url):
     allsides = pd.read_csv('backend_logic/allsides.csv')
+    dict = {'center': 50, 'left-center': 25, 'left': 5, 'right-center': 75, 'right': 95}
     parsed = tldextract.extract(url)
     website = parsed.domain
     allsides_vals = allsides.values
@@ -14,10 +15,9 @@ def get_allsides(url):
         rows = [row for row in allsides_vals if website.lower()[:3] in row[0].lower() and website.lower()[:3] != 'the']
     if rows != []:
         rows.sort(key = lambda x: x[2],reverse=True)
-        return rows[0]
+        return jsonify({'allsides_rating': dict[rows[0][1]], 'message': 'Success'})
     distances = [jellyfish.jaro_similarity(website.lower(), x.lower()) for x in sources]
     loc = distances.index(max(distances))
     row = allsides_vals[loc]
-    dict = {'center':50,'left-center':25,'left':5,'right-center':75,'right':95}
-    print(row,dict[row[1]])
+
     return jsonify({'allsides_rating': dict[row[1]], 'message': 'Success'})
